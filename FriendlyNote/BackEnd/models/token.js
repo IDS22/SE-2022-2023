7d813d08ca6de035110d5fef3f386e89c1255e99
Dibo002 <102266408+Dibo002@users.noreply.github.com>
2023-02-18 10:42:03 +0100

    Delete FriendlyNote/BackEnd directory

--- FriendlyNote/BackEnd/models/token.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const TokenSchema = new mongoose.Schema({
-    token: {type: String, required: true, unique: true},
-    createdAt: {type: Date, required: true, default: Date.now}
-});
-
-const Token = mongoose.model('Token', TokenSchema)
-module.exports = Token
