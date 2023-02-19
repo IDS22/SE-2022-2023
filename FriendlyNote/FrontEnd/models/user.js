92e4893eb9f564e24ea7712c93ac0f43d3e902e7
Dibo002 <102266408+Dibo002@users.noreply.github.com>
2023-02-19 16:34:57 +0100

    Delete FriendlyNote/FrontEnd directory

--- FriendlyNote/FrontEnd/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const UserSchema = new mongoose.Schema({
-    mail: {type: String, required: true, unique: true},
-    password: {type: String, required: true, unique: false, index: {unique:false, name:'my_custom_index'}}
-});
-
-const User = mongoose.model('User', UserSchema)
-module.exports = User
